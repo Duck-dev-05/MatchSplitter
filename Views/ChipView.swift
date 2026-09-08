636e0ca0c5af27ec1cb73a69a11a0f1a65de486f
Minh Duc <minhduc.dev2005@gmail.com>
2026-09-08 15:38:36 +0700

    Commit of ChipView

---
+++ Views/ChipView.swift
@@ -0,0 +1,24 @@
+import SwiftUI
+
+struct ChipView: View {
+    var text: String
+    
+    var body: some View {
+        Text(text)
+            .font(.system(size: 14, weight: .medium, design: .rounded))
+            .foregroundColor(Color(red: 0.05, green: 0.32, blue: 0.55)) // Dark blue text
+            .padding(.horizontal, 14)
+            .padding(.vertical, 8)
+            .background(Color(red: 0.93, green: 0.87, blue: 0.85)) // Light beige/gray background
+            .clipShape(Capsule())
+    }
+}
+
+struct ChipView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChipView(text: "Category")
+            .previewLayout(.sizeThatFits)
+            .padding()
+            .previewDisplayName("Light Theme Pill")
+    }
+}
