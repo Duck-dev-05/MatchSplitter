29551a9596ee080e28b6246a213b21024392573d
Minh Duc <minhducworking05@gmail.com>
2026-09-17 16:16:11 +0700

    refactor: prefix Group with SwiftUI module

--- MatchSplitterApp.swift
+++ MatchSplitterApp.swift
@@ -13,7 +13,7 @@ struct MatchSplitterApp: App {
     var body: some Scene {
         WindowGroup {
             GeometryReader { geo in
-                Group {
+                SwiftUI.Group {
                     if groupViewModel.currentUser == nil {
                         LandingView()
                     } else {
